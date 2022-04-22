0a25db4f7bc99ee1019cd71d86fb301177ea9323
Gary Barker <garybarker.19894@gmail.com>
2022-04-22 15:45:18 -0400

    all tests pass

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,18 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledCharacter = styled.h4`
+  width: 100%;
+  color: 'black';
+  text-shadow: 1px 1px 5px #fff;
+`
+
+export default function Character(props) {
+
+    return (
+        <div>{props.characters.map((character, idx) => <StyledCharacter key={idx}>{character.name}</StyledCharacter>)}</div>
+    )
+};
+
+
